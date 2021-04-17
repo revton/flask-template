b3cb20ac867357ad3a1d1919a952d0e615d5a9d8
Revton Braga <revtonbr@gmail.com>
2021-04-17 16:15:26 -0300

    Adicionar DebugToolbar ao projeto #10

--- tests/test_flask_app.py
+++ tests/test_flask_app.py
@@ -1,4 +1,4 @@
 def test_app_flask(test_client):
     response = test_client.get("/")
     assert response.status_code == 200
-    assert response.data == b"Index"
+    assert response.data == b'<html><body>Teste</body></html>'
