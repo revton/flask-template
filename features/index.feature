b544bd4f05ca25536c78eaae5250c802f62ff1be
Revton Braga <revtonbr@gmail.com>
2021-04-21 15:53:03 -0300

    Incluído rota para tela de admin #31

--- features/index.feature
+++ features/index.feature
@@ -2,5 +2,5 @@ Feature: Index Screen
 
   Scenario: Open the index screen
     Given flask-template is setup
-    When open the page
+    When open the page index
     Then should see the message "FLASK-TEMPLATE"
--- features/steps/steps.py
+++ features/steps/steps.py
@@ -6,13 +6,26 @@ def step_impl(context):
     assert context.server
 
 
-@when(u"open the page")
+@when(u"open the page index")
 def step_impl(context):
     context.browser.get("http://127.0.0.1:5001/")
     assert context.browser.current_url == "http://127.0.0.1:5001/"
+
+
+@when(u'open the page index of admin')
+def step_impl(context):
+    context.browser.get("http://127.0.0.1:5001/admin")
+    assert context.browser.current_url == "http://127.0.0.1:5001/admin/"
 
 
 @then(u'should see the message "{message}"')
 def step_impl(context, message):
     assert context.browser.current_url == "http://127.0.0.1:5001/"
     assert message in context.browser.page_source
+
+
+@then(u'should see the link to "{text_link}"')
+def step_impl(context, text_link):
+    assert context.browser.current_url == "http://127.0.0.1:5001/admin/"
+    assert context.browser.find_element_by_link_text(text_link)
+
