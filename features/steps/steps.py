587e849ff3ddff0362824a543e5487f638403af9
Revton Braga <revtonbr@gmail.com>
2021-04-21 02:18:43 -0300

    Incluído Selenium para testes de comportamentos #28

--- features/steps/steps.py
+++ features/steps/steps.py
@@ -3,15 +3,16 @@ from behave import given, then, when
 
 @given(u"flask-template is setup")
 def step_impl(context):
-    assert context.client
+    assert context.server
 
 
 @when(u"open the page")
 def step_impl(context):
-    context.page = context.client.get("/")
-    assert context.page
+    context.browser.get("http://127.0.0.1:5001/")
+    assert context.browser.current_url == "http://127.0.0.1:5001/"
 
 
 @then(u'should see the message "{message}"')
 def step_impl(context, message):
-    assert message in str(context.page.data)
+    assert context.browser.current_url == "http://127.0.0.1:5001/"
+    assert message in context.browser.page_source
